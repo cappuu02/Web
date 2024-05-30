03b3a392c7945b77b6c4eecb332247253dd41f19
cappuu02 <luca.capuccini@studenti.unipg.it>
2024-05-30 16:26:27 +0200

    responsive Account ok finished

--- src/components/Security.js
+++ src/components/Security.js
@@ -4,14 +4,14 @@ import style from '../profile.module.css'
 function Security(){
     return(
         <>
-        <div>
                 <div id={style.box_profile2} >
                     <h2 id={style.title_2}>Email Changer</h2>
                     <p style={{textAlign: 'center'}}>If you want to change your email address click the button below to start!</p>
                     <input id={style.input_profilo2} type="text" value="lucacapuccini13@gmail.com" readonly/>                    
                     <button id={style.button_email_reset2} type="button" class="btn btn-danger">Reset</button>
-                </div>
-                <div id={style.box_profile3} >
+
+                    <hr></hr>
+                
                     <h2 id={style.title_2}>Password Changer</h2>
                     <p style={{textAlign: 'center'}}>If you want to change your account password click the button and wait code authenticator</p>
                     <div>
@@ -19,19 +19,16 @@ function Security(){
                         <button id={style.button_psw_reset2} type='button' class="btn btn-danger">Reset</button>
                     </div>
 
-                </div>
-                <div id={style.box_profile4} >
+                    <hr></hr>
+                
+                
                     <h2 id={style.title_2}>Phone Number Changer</h2>
                     <p style={{textAlign: 'center'}}>If you want to change tour account's telephone number follow the steps below</p>
                     <div>
                         <input id={style.input_profilo3} type="text" value="Readonly 1" readonly/>
                         <button id={style.button_psw_reset2} type='button' class="btn btn-danger">Reset</button>
                     </div>
-
                 </div>
-        </div>
-
-        
         </>
     )
 }
