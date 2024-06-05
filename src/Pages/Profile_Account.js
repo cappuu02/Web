9fdfd46ffdca8f74fb1ee9be07b47894ef2b8969
SylweKra <skrasinki62@gmail.com>
2024-06-05 22:39:06 +0200

    sas

---
+++ src/Pages/Profile_Account.js
@@ -0,0 +1,47 @@
+
+import React, { useState, useEffect } from 'react';
+import NavbarP from '../components/Navbar_Profile';
+import Account from '../components/Account'
+import Navbar from '../components/navbar';
+import style from '../profile.module.css'
+import NavbarPR from '../components/Navbar_ProfiloR';
+
+
+    function Profile() {
+        const [showButton, setShowButton] = useState(false);
+
+        useEffect(() => {
+          const handleMouseMove = (event) => {
+            if (event.clientX < 50) {  // Aggiorna questa soglia a 50 pixel
+              setShowButton(true);
+            } else {
+              setShowButton(false);
+            }
+          };
+      
+          window.addEventListener('mousemove', handleMouseMove);
+      
+          return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+          };
+        }, []);
+      
+    return (
+        <>  
+            <div id={style.body_profilo}>
+                <Navbar />
+                <NavbarP />
+                <div className="App">
+             <NavbarPR showButton={showButton} />
+             </div>
+                <Account />
+            </div>
+        </>
+    );
+  }
+
+  export default Profile;
+
+
+
+
