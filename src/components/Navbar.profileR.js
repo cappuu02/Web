4ec5752b726339be1031ca519e9c5be53ff34bae
SylweKra <skrasinki62@gmail.com>
2024-06-05 22:47:16 +0200

    sas

---
+++ src/components/Navbar.profileR.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './NavbarR.css';
+
+function Navbar({ showButton }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`navbar-container ${isOpen ? 'open' : ''}`}>
+      {showButton && (
+        <button className="open-button" onClick={handleButtonClick}>
+          {isOpen ? 'Close' : 'Open'}
+        </button>
+      )}
+      <nav className={`navbar ${isOpen ? 'visible' : 'hidden'}`}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
